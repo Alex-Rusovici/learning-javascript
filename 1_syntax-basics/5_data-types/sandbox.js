ecb10abaeddeb7a4986742f031fe22ca0bfb00e5
Alexandru Rusovici <alex.rusovici@yahoo.com>
2022-11-06 22:41:24 +0200

    1 - data-types
    1 - data-types

---
+++ 1_syntax-basics/5_data-types/sandbox.js
@@ -0,0 +1,28 @@
+// string
+console.log('hello, world');
+
+let email = 'alex@frontend-dev.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Alexandru';
+let lastName = 'Rusovici';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string mehtods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
